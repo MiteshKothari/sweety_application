b727107bcbb7dbc2fdcc5de6952265de9d7b18f9
MiteshKothari <mitesh.kothari10@yahoo.com>
2019-08-31 13:19:02 +0530

    sweety application

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    # render json: users.to_json(response_attributes), status: :ok
+  end
+
+  def show
+    @user = User.find params[:id]
+    @options = User.report_options
+    @readings = @user.get_report params
+    @todays_readings = @user.todays_readings
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user
+    else
+      render json: user.to_json(
+        methods: [ :error_messages ]
+      )
+    end
+  end
+
+  def update
+    user = User.find params[:id]
+    if user.update(user_params)
+      redirect_to user
+    else
+      render json: user.to_json(
+        methods: [ :error_messages ]
+      )
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :phone)
+  end
+
+  def response_attributes
+    {
+      only: [ :name, :email, :phone, :id ],
+      include: {
+        readings: {
+          only: [:date, :reading]
+        }
+      }
+    }
+  end
+end
